4f2ce034d2bdc7ba06ccdad0c65b2ee9ea8554ba
Kyle Wong <kylewong2510@gmail.com>
2020-05-17 08:32:07 -0600

    Add file for charts

--- src/scripts.js
+++ src/scripts.js
@@ -44,157 +44,159 @@ displayAvgSleepQualForUser('2019/06/15');
 const weekSleepHoursId = document.getElementById("week-sleep-hours").getContext("2d");
 const weekSleepQualId = document.getElementById("week-sleep-quality").getContext("2d");
 
+charts.weeklySleepHoursChart(weekSleepHoursId, weekSleepHours)
+// let weeklySleepHoursChart = new Chart(weekSleepHoursId, {
+//   type: "line",
+//   data: {
+//     labels: ["Day 1", " ", " ", " ", " ", " ", "Day 7"],
+//     datasets: [{
+//       label: "Weekly Sleep Hours",
+//       data: weekSleepHours,
+//       fill: false,
+//       backgroundColor: [
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//       ],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }]
+//     },
+//     maintainAspectRatio: false,
+//     responsive: true,
+//     legend: {
+//       display: false
+//     },
+//     title: {
+//       display: true,
+//       text: "Weekly Sleep Hours",
+//     }
+//   }
+// });
 
-let weeklySleepHoursChart = new Chart(weekSleepHoursId, {
-  type: "line",
-  data: {
-    labels: ["Day 1", " ", " ", " ", " ", " ", "Day 7"],
-    datasets: [{
-      label: "Weekly Sleep Hours",
-      data: weekSleepHours,
-      fill: false,
-      backgroundColor: [
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-      ],
-      borderColor: [
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    },
-    maintainAspectRatio: false,
-    responsive: true,
-    legend: {
-      display: false
-    },
-    title: {
-      display: true,
-      text: "Weekly Sleep Hours",
-    }
-  }
-});
-
-let weeklySleepQualChart = new Chart(weekSleepQualId, {
-  type: "line",
-  data: {
-    labels: ["Day 1", " ", " ", " ", " ", " ", "Day 7"],
-    datasets: [{
-      label: "Weekly Sleep Quality",
-      data: weekSleepQuality,
-      fill: false,
-      backgroundColor: [
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-      ],
-      borderColor: [
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    },
-    maintainAspectRatio: false,
-    responsive: true,
-    legend: {
-      display: false
-    },
-    title: {
-      display: true,
-      text: "Weekly Sleep Quality",
-    }
-  }
-});
+charts.weeklySleepQualChart(weekSleepQualId, weekSleepQuality)
+// let weeklySleepQualChart = new Chart(weekSleepQualId, {
+//   type: "line",
+//   data: {
+//     labels: ["Day 1", " ", " ", " ", " ", " ", "Day 7"],
+//     datasets: [{
+//       label: "Weekly Sleep Quality",
+//       data: weekSleepQuality,
+//       fill: false,
+//       backgroundColor: [
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//       ],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }]
+//     },
+//     maintainAspectRatio: false,
+//     responsive: true,
+//     legend: {
+//       display: false
+//     },
+//     title: {
+//       display: true,
+//       text: "Weekly Sleep Quality",
+//     }
+//   }
+// });
 
 const weekOuncesDrank = hydration.getWeeklyWater('2019/06/15')
 
-const weekHydration = document.getElementById("week-hydration").getContext("2d");
+const weekHydrationId = document.getElementById("week-hydration").getContext("2d");
 
-const weekHydrationChart = new Chart(weekHydration, {
-  type: "line",
-  data: {
-    labels: ["Day 1", " ", " ", " ", " ", " ", "Day 7"],
-    datasets: [{
-      label: "Weekly Hydration",
-      data: weekOuncesDrank,
-      fill: false,
-      backgroundColor: [
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-      ],
-      borderColor: [
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(153, 102, 255, 1)",
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    },
-    maintainAspectRatio: false,
-    responsive: true,
-    legend: {
-      display: false
-    },
-    title: {
-      display: true,
-      text: "Weekly Ounces Drank",
-    }
-  }
-});
+charts.weeklyHydrationChart(weekHydrationId, weekOuncesDrank)
+// const weekHydrationChart = new Chart(weekHydration, {
+//   type: "line",
+//   data: {
+//     labels: ["Day 1", " ", " ", " ", " ", " ", "Day 7"],
+//     datasets: [{
+//       label: "Weekly Hydration",
+//       data: weekOuncesDrank,
+//       fill: false,
+//       backgroundColor: [
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//         "rgba(153, 102, 255, 0.2)",
+//       ],
+//       borderColor: [
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//         "rgba(153, 102, 255, 1)",
+//       ],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }]
+//     },
+//     maintainAspectRatio: false,
+//     responsive: true,
+//     legend: {
+//       display: false
+//     },
+//     title: {
+//       display: true,
+//       text: "Weekly Ounces Drank",
+//     }
+//   }
+// });
 
 const todayHydration = document.getElementById("today-hydration")
 
